ff72b69b0ccc2666acbacd8de2c6a187cb4d37ae
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-07-31 19:47:12 -0500

    update not working

--- Develop/routes/workout-api-routes.js
+++ Develop/routes/workout-api-routes.js
@@ -1,16 +1,29 @@
 var db = require("../models");
+const mongoose = require('mongoose');
 //const mongojs = require("mongojs");
 
 module.exports = function(app) {
-    app.post("/api/workouts/",({body},res) => {
+    //Create a new workout
+    app.post("/api/workouts",({body},res) => {
+        db.Workout.create(body)
+        .then(Workout => {
+            res.json(Workout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    });
+
+    //Create a new exercise and insert it in the workout collection
+    /*app.post("/api/workouts",({body},res) => {
         console.log("BODY",body);
-        //let exerciseId =  mongojs.ObjectId(req.params.id);
-        db.Exercise.create(body.exercise)
+        db.Exercise.create(body)
         .then(dbExercise => {
+            console.log(dbExercise)
             console.log(dbExercise._id);
             console.log(body.workoutId);
             db.Workout.findByIdAndUpdate(body.workoutId,{
-                $push: {exercises: "HOLA"}
+                $push: {exercises: body.exercise}
                 //test:"HOLA"
             },{new: true}).then(dbWorkout => {
                 console.log("WORKOUT", dbWorkout);
@@ -19,23 +32,44 @@ module.exports = function(app) {
             .catch(err => {
                 res.json(err);
             });
+            res.json(dbExercise);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });*/
+
+    //Get the workout information
+    app.get("/api/workout",(req,res) => {
+        //let id = mongoose.Types.ObjectId(req.params.id);
+        db.Workout.find()
+        .then(Workout => {
+            //console.log(Workout);
+            //console.log(typeof Workout[0]._id);
+            res.json(Workout);
         })
         .catch(err => {
             res.json(err);
         });
     });
 
-    app.get("/api/workouts/",(req,res) => {
-        db.Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+    //Updte the exercise array 
+    app.put("/api/workouts/:id",(req,res)=> {
+        let id = mongoose.Types.ObjectId(req.params.id);
+        console.log(id);
+        //console.log(typeof id);
+        console.log(req.body);
+        db.Workout.findByIdAndUpdate(id, {$push: {exercises: req.body}}, {new: true, upsert: true})
+        .then(Exercise => {
+            console.log(Exercise);
+            res.json(Exercise);
         })
         .catch(err => {
             res.json(err);
-        });
+        })
     });
 
-    app.put("/api/workouts/:id",(req,res) => {
+    /*app.put("/api/workouts/:id",(req,res) => {
         let id = req.params.id;
         console.log(id);
         db.Workout.updateOne(
@@ -54,7 +88,7 @@ module.exports = function(app) {
             } else {
                 res.json(data);
             }
-        });
+        });*/
         /*db.Workout.findOneAndUpdate({},
             {
                 _id: id
@@ -71,5 +105,5 @@ module.exports = function(app) {
                     res.json(data);
                 }
             });*/
-    });
+    //});
 };
